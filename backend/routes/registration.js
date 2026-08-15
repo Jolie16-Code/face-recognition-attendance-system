9c8d581cde7f95b484e93a03153c501329f05cd9
Debanjali Adhikary <debanjaliad@gmail.com>
2026-08-15 21:35:37 +0530

    Noilmailer updated

--- backend/routes/registration.js
+++ backend/routes/registration.js
@@ -18,11 +18,16 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 connectDb()*/
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail', // Use 'gmail', 'outlook', 'yahoo', or a custom host
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
-        user: process.env.EMAIL_USER, // Your admin email from .env
-        pass: process.env.EMAIL_PASS  // Your app password or regular password from .env
-    }
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000
 });
 transporter.verify((error, success) => {
     if (error) {
